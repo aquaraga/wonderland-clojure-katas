ac65c8f0242e27e2f20b9088e1474f450323c7cf
aquaraga <aquaraga@gmail.com>
2018-03-15 02:11:38 +0530

    Alphabet cipher encode

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,16 @@
 (ns alphabet-cipher.coder)
 
+(def alphabets "abcdefghijklmnopqrstuvwxyz")
+
+(defn encode-substitute [row col]
+  (let [not-row (partial not= row)
+        subst-str (take 26 (drop-while not-row (cycle (sequence alphabets))))
+        subst-map (zipmap alphabets subst-str)]
+    (get subst-map col)))
+
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str (map encode-substitute (cycle keyword) message)))
 
 (defn decode [keyword message]
   "decodeme")
