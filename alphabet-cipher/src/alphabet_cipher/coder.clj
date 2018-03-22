1249ba8cef9172718ee7f4ed914101dceaa8e94e
aquaraga <aquaraga@gmail.com>
2018-03-22 23:39:37 +0530

    Decode kata

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -8,12 +8,19 @@
         subst-map (zipmap alphabets subst-str)]
     (get subst-map col)))
 
+(defn decode-substitute [row col]
+  (let [not-row (partial not= row)
+        subst-str (take 26 (drop-while not-row (cycle (sequence alphabets))))
+        subst-map (zipmap subst-str alphabets)]
+    (get subst-map col)))
+
 
 (defn encode [keyword message]
   (apply str (map encode-substitute (cycle keyword) message)))
 
+
 (defn decode [keyword message]
-  "decodeme")
+  (apply str (map decode-substitute (cycle keyword) message)))
 
 (defn decipher [cipher message]
   "decypherme")
