0e41d444eb78e9a564a70d5d426fae7c34e4c142
aquaraga <aquaraga@gmail.com>
2018-03-23 15:26:07 +0530

    wonderland number solution

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,16 @@
 (ns wonderland-number.finder)
 
+
+(defn hasAllTheSameDigits? [n1 n2]
+  (let [s1 (set (str n1))
+        s2 (set (str n2))]
+    (= s1 s2)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (let [possible (range 100000 (quot 999999 6))
+        all-multiples-fns (map #(partial * %) (range 2 7))
+        all-multiples-of-num (fn [num] (map #(% num) all-multiples-fns))
+        same-digits-for-num
+          (fn [num] (every? #(hasAllTheSameDigits? % num) (all-multiples-of-num num)))]
+    (first (filter #(same-digits-for-num %) possible)))
+  )
